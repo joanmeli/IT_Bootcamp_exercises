279ab4762a982868133bd94ffab15ffe6d3efd7d
joanmeli <joan.silva@mercadolivre.com>
2022-03-14 17:53:26 -0300

    Adcionado metodo calcularIMC

--- Modulo_2/src/Pratica_1_java_II/Pessoa.java
+++ Modulo_2/src/Pratica_1_java_II/Pessoa.java
@@ -23,4 +23,34 @@ public class Pessoa {
         this.peso = peso;
         this.altura = altura;
     }
+
+    public int calcularIMC(){
+        double altura = this.altura/100;
+        double imc = this.peso/(altura*2);
+        if(imc < 20){
+            return -1;
+        }
+        if( imc > 25){
+            return 1;
+        }
+        return 0;
+    }
+
+    public boolean ehMaiorIdade(){
+        if (this.idade > 18){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return "\n\nPessoa {" +
+                "ID =" + this.ID +
+                ", Nome =" + this.nome+
+                ", Idade =" + this.idade+ " anos" +
+                ", Peso =" + this.peso+
+                ", Altura =" + this.altura+ " cm" +
+                '}';
+    }
 }
