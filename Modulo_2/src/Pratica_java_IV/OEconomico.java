33476d1fc7779a9534c2ad1857bc300b955f35ac
joanmeli <joan.silva@mercadolivre.com>
2022-03-16 12:01:12 -0300

    Criacao dos clientes

---
+++ Modulo_2/src/Pratica_java_IV/OEconomico.java
@@ -0,0 +1,16 @@
+package Pratica_java_IV;
+
+import Pratica_java_IV.Entidades.Cliente;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class OEconomico {
+    public static void main(String[] args) {
+        ArrayList<Cliente> clientes = new ArrayList<Cliente>(Arrays.asList(
+                new Cliente(1, "Joan", "Silva"),
+                new Cliente(2, "Davi", "Santos"),
+                new Cliente(3, "Késia", "Silva")
+        ));
+    }
+}
